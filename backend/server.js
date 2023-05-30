433abff5785670fb68a095632cbab95fe0582148
Ivo Alves <ivo420alves@gmail.com>
2023-05-30 11:39:37 +0100

    MongoDB database setup

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
